acf8505aa050c46465d08bc1af5b1a80e6f0ba83
= <thawhtooin@gmail.com>
2023-01-02 20:26:51 +0630

    done week3.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,20 @@
 @extends('layout')
 
 @section('content')
-  <h2>Home Page</h2>
-  @foreach($data as $key => $value)
-  {{$key . ' = ' . $value}}
-  @endforeach
+  <div class="container">
+    <div class="card">
+      <div class="card-header" style="text-align:center;">
+        Contents
+      </div>
+      <div class="card-body">
+        @foreach( $data as $post)
+        <div>
+          <h5 class="card-title">{{ $post->name }}</h5>
+          <p class="card-text">{{ $post->description }}</p>
+          <a href="#" class="btn btn-primary">View</a>
+        </div>
+        @endforeach
+      </div>
+    </div>
+  </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,12 +3,10 @@
   <head>
     <meta charset="utf-8">
     <title>Ap Laravel</title>
-    <h2>Laravel Class</h2>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <h2 style="text-align:center;">Simple Blog Site</h2>
   </head>
   <body>
-    <li><a href="/">Home Page</a></li>
-    <li><a href="about">About Page</a></li>
-    <li><a href="contact">Contact Page</a></li>
     @yield('content')
   </body>
 </html>
